ad3fd8f3c024b370ed6fd7834032b83a8ff58b84
MartinSchoeler <martinschoeler8@gmail.com>
2026-04-20 16:04:03 -0300

    fix: Adjust unread marker message read

--- apps/meteor/client/lib/chats/readStateManager.ts
+++ apps/meteor/client/lib/chats/readStateManager.ts
@@ -24,12 +24,6 @@ export class ReadStateManager extends Emitter {
 		return this.rid;
 	}
 
-	// TODO: Use ref to get unreadMark
-	// private unreadMark?: HTMLElement;
-	private get unreadMark() {
-		return document.querySelector<HTMLElement>('.rcx-message-divider--unread');
-	}
-
 	public onUnreadStateChange = (callback: () => void): (() => void) => {
 		return this.on('unread-state-change', callback);
 	};
@@ -113,12 +107,10 @@ export class ReadStateManager extends Emitter {
 		};
 	};
 
-	private isUnreadMarkVisible(): boolean {
-		if (!this.unreadMark) {
-			return false;
-		}
+	private isUnreadMarkVisible: () => boolean = () => false;
 
-		return this.unreadMark.offsetTop > (this.unreadMark.offsetParent?.scrollTop || 0);
+	public setIsUnreadMarkVisibleCallback(callback: () => boolean) {
+		this.isUnreadMarkVisible = callback;
 	}
 
 	// This will only mark as read if the unread mark is visible
@@ -132,7 +124,7 @@ export class ReadStateManager extends Emitter {
 			return;
 		}
 
-		if (this.unreadMark && !this.isUnreadMarkVisible()) {
+		if (!this.isUnreadMarkVisible()) {
 			return;
 		}
 		// if there are unloaded unread messages, don't mark as read
